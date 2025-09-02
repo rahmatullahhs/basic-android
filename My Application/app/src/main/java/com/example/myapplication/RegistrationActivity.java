4ce6dd542f315dfb410007eef224c19f1f757def
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-09-03 01:05:31 +0600

    aaa

--- My Application/app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ My Application/app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,44 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrationActivity extends AppCompatActivity {
+private EditText mEmail ;
+private EditText mPass;
+private Button btnRed;
+private TextView mSingin;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+registrition();
+
     }
+    private void registrition(){
+
+        mEmail=findViewById(R.id.email_register);
+        mPass=findViewById(R.id.password_register);
+        btnRed=findViewById(R.id.btn_register);
+        mSingin=findViewById(R.id.signup_reg);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
